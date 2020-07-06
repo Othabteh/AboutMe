e25d91b394f03113b4c79ffa8bd8eb80a8977731
oalthabteh <o.thabteh@gmail.com>
2020-07-06 15:22:06 +0300

    creating aboutme repo

---
+++ js/app.js
@@ -0,0 +1,71 @@
+'use strict';
+var userName = prompt('What is your name?');
+alert('Hello ' + userName +' welcome to my website');
+
+alert('Now you will have five questions for you to answer. The questions are facts and basic information about me. ');
+alert('Please answer the following question by typing yes or no');
+
+
+
+    var question1=prompt('Do you think that I like watching movies?');
+    question1=question1.toLowerCase();
+    if( question1==='yes'){
+        alert('you are right i like waching movies');
+    }else if(question1==='no'){
+        alert('Nop... you are wrong ');
+    }else{ 
+        alert('please answer by yes or no ');
+    }
+
+
+
+
+    var question2=prompt('Do you think that I like drinking coffe?');
+    question2=question2.toLowerCase();
+    if(question2==='yes'){
+        alert('you are right i like drinking coffe');
+    }else if(question2==='no'){
+        alert('Nop... you are wrong ');
+    }else{ 
+        alert('please answer by yes or no ');
+    }
+
+
+
+
+
+    var question3=prompt('Do you think that I like swimming?');
+    question3=question3.toLowerCase();
+    if(question3==='yes'){
+        alert('you are right i like swimming');
+    }else if(question3==='no'){
+        alert('Nop... you are wrong ');
+    }else{ 
+        alert('please answer by yes or no ');
+    }
+
+
+
+
+
+    var question4=prompt('Do you think that I like noddle?');
+    question4=question4.toLowerCase();
+    if(question4==='yes'){
+        alert('Nop... you are wrong');
+    }else if(question4==='no'){
+        alert('you are right i do not like noddle ');
+    }else{ 
+        alert('please answer by yes or no ');
+    }
+
+
+    var question5=prompt('Do you think that I like playing football?');
+    question5=question5.toLowerCase();
+    if(question5==='yes'){
+        alert('Nop... you are wrong');
+    }else if(question5==='no'){
+        alert('you are right i do not like playing football');
+    }else{ 
+        alert('please answer by yes or no ');
+    }
+
